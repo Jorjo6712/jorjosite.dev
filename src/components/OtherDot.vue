fba7ec053029b22e40d71e9da6fbfb4b83d24a56
JorjoWasTaken <kiro.nicolov@gmail.com>
2024-01-25 23:25:59 +0100

    small change for ngnix

--- src/components/OtherDot.vue
+++ src/components/OtherDot.vue
@@ -1,15 +1,10 @@
 <template>
-<div class="flex justify-center items-center mt-4">
-  <button class="rounded-2xl focus:outline-none focus:ring-0 focus:ring-offset-2 text-white hover:bg-white hover:text-black hover:transition ease-in-out duration-300 px-6 py-3" @click="getComments">Get comments</button>
-    <div class="flex flex-row justify-center items-centerw-1/4">
-    <ul class="flex-col">
-      <li class="mt-4" v-for="comment in comments" :key="comment.id" data-test="comment">
-        <h1 class="py-4 bg-white text-black rounded-xl mb-0.5">{{ comment.author }}</h1>
-        <h1 class="py-4 bg-white text-black rounded-xl mb-0.5">{{ comment.message }}</h1>
-        <h1 class="py-4 bg-white text-black rounded-xl mb-0.5">{{ comment.timestamp }}</h1>
-      </li>
-    </ul>
-  </div>
+<div class="">
+  <li class="" v-for="comment in comments" :key="comment.id" data-test="comment">
+    <h1 class="">{{ comment.author }}</h1>
+    <h1 class="">{{ comment.message }}</h1>
+    <h1 class="">{{ comment.timestamp }}</h1>
+  </li>
 </div>
 </template>
     
@@ -29,7 +24,7 @@ export default {
   methods: {
     async getComments() {
       try {
-        const response = await axios.get("http://172.18.100.113:3000/comments")
+        const response = await axios.get("http://10.0.0.223:3000/comments")
         this.comments = response.data
       } catch (error) {
         console.error('Error fetching comments:', error)
